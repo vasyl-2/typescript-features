4fe512fd9b837d90c774fe5b91702c8ee603029e
Vasiliy <dvv03122014@gmail.com>
2023-12-16 23:34:45 +0300

    try

--- src/app.ts
+++
@@ -1,7 +0,0 @@
-import { greet } from './function-overloading/first/file';
-
-const stingParamResponse = greet('5');
-const numberParamResponse = greet(5, 5);
-
-console.log('RES_____1', stingParamResponse);
-console.log('RES_____2', numberParamResponse);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-// https://stackoverflow.com/questions/65584688/trying-to-declare-a-function-getting-error-ts2384-overload-signatures-must-all
-
 export  function greet(person: string): string;
 export  function greet(person: number, age: number): string;
 
-// Implement the function elsewhere (e.g., in another file)
 export function greet(person: string | number, age?: number): string {
     if (age !== undefined) {
         return `Hello, ${person}! You are ${age} years old.`;
@@ -11,5 +8,3 @@ export function greet(person: string | number, age?: number): string {
         return `Hello, ${person}!`;
     }
 }
-
-
